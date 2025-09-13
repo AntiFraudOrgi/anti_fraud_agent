11042639d566e7c9e2f8655ff6e684288eac0342
Harry <86918643+bear7066@users.noreply.github.com>
2025-09-13 12:28:01 +0800

    fix: remove apikey

--- anti_fraud/agents/test.py
+++ anti_fraud/agents/test.py
@@ -5,8 +5,7 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
 import os
 
-os.environ["OPENAI_API_KEY"] = "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"
-
+os.environ["OPENAI_API_KEY"] = ""
 
 raw_documents = TextLoader('FVE.').load()
 text_splitter = CharacterTextSplitter(chunk_size = 500, chunk_overlap = 0)
